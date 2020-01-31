bb8fdfcfaf86b2ad8762debbb2286f074c3c7cd2
Jose Anibal Rodriguez <jose_an_rodriguez@claro.com.do>
2020-01-31 11:57:19 -0400

    Removed warnings from document validation class

--- src/main/java/org/dominisoft/scrumdev/claro2020/domain/DopNationalIdentificationNumber.java
+++ src/main/java/org/dominisoft/scrumdev/claro2020/domain/DopNationalIdentificationNumber.java
@@ -7,62 +7,68 @@ import org.dominisoft.scrumdev.claro2020.domain.exceptions.DocumentInvalidExcept
  */
 public final class DopNationalIdentificationNumber {
 
-    private final String value;
+  private final String value;
 
-    /**
-     * Validates the given value ensuring only valid data can be used to create instances of this class.
-     *
-     * @param document National Identification Number
-     */
+  /**
+   * Validates the given value ensuring only valid data can be used to create instances of this class.
+   *
+   * @param document National Identification Number
+   */
 
-    public DopNationalIdentificationNumber(final String document) {
-        validateIfDocEmpty(document);
-        validateIfInvalid("01234567891".equals(document), "El documento es inválido");
-        validateIfInvalid(!document.matches("[0-9]+"), "El documento no debe contener solo numeros");
-        validateIfInvalid(document.length() != 11, "El documento debe tener solo 11 caracteres");
-        validateIfInvalid(!validateDocument(document), "Debe ser un documento válido");
-        value = document;
+  public DopNationalIdentificationNumber(final String document) {
+    validateIfDocEmpty(document);
+    validateIfInvalid("01234567891".equals(document), "El documento es inválido");
+    validateIfInvalid(!document.matches("[0-9]+"), "El documento no debe contener solo numeros");
+    validateIfInvalid(document.length() != 11, "El documento debe tener solo 11 caracteres");
+    validateIfInvalid(!validateDocument(document), "Debe ser un documento válido");
+    value = document;
+  }
+
+  private void validateIfInvalid(boolean val, String s) {
+    if (val) {
+      throw new DocumentInvalidException(s);
     }
+  }
 
-    private void validateIfInvalid(boolean val, String s) {
-        if (val) {
-            throw new DocumentInvalidException(s);
-        }
+  private void validateIfDocEmpty(String document) {
+    if (document == null || document.isBlank()) {
+      throw new IllegalArgumentException("El Documento es requerido");
     }
+  }
 
-    private void validateIfDocEmpty(String document) {
-        if (document == null || document.isBlank()) {
-            throw new IllegalArgumentException("El Documento es requerido");
-        }
+  /**
+   * Method for validating document.
+   *
+   * @param document is the document id for voting.
+   * @return a boolean with the validation result.
+   */
+  public static boolean validateDocument(String document) {
+    int suma = 0;
+    final char[] peso = {'1', '2', '1', '2', '1', '2', '1', '2', '1', '2'};
+    if (document == null || document.length() != 11) {
+      return false;
     }
-
-    public static boolean validateDocument(String document) {
-        int suma = 0;
-        final char[] peso = {'1', '2', '1', '2', '1', '2', '1', '2', '1', '2'};
-        if ((document == null) || (document.length() != 11)) {
-            return false;
-        }
-        try {
-            Long.parseLong(document);
-        } catch (Exception e) {
-            return false;
-        }
-        for (int i = 0; i < 10; i++) {
-            int a = Character.getNumericValue(document.charAt(i));
-            int b = Character.getNumericValue(peso[i]);
-            char[] multiplier = Integer.toString(a * b).toCharArray();
-            if (multiplier.length > 1) {
-                a = Character.getNumericValue(multiplier[0]);
-                b = Character.getNumericValue(multiplier[1]);
-            } else {
-                a = 0;
-                b = Character.getNumericValue(multiplier[0]);
-            }
-            suma = suma + a + b;
-        }
-        int digit = (10 - (suma % 10)) % 10;
-        return digit == Character.getNumericValue(document.charAt(10));
+    try {
+      Long.parseLong(document);
+    } catch (Exception e) {
+      return false;
     }
+    for (int i = 0; i < 10; i++) {
+      int a = Character.getNumericValue(document.charAt(i));
+      int b = Character.getNumericValue(peso[i]);
+      char[] multiplier = Integer.toString(a * b).toCharArray();
+      if (multiplier.length > 1) {
+        a = Character.getNumericValue(multiplier[0]);
+        b = Character.getNumericValue(multiplier[1]);
+      } else {
+        a = 0;
+        b = Character.getNumericValue(multiplier[0]);
+      }
+      suma = suma + a + b;
+    }
+    int digit = (10 - (suma % 10)) % 10;
+    return digit == Character.getNumericValue(document.charAt(10));
+  }
 
   /**
    * Returns wrapped value.
